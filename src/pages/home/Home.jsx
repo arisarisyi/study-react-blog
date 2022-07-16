383bd0afcd687297f8ce4a158d03cb46004450e4
Imam Al Arisyi <alarisyi@gmail.com>
2022-07-16 21:18:17 +0700

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.css"
+import axios from "axios"
 
 export default function Home() {
+  const [posts, setPost] = useState([])
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await axios.get("/posts")
+      console.log(response)
+    }
+    fetchPost()
+  }, [])
   return (
     <>
       <Header />
